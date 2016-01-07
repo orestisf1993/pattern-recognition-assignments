809e7ae350eeb910e0288f7814a63e09e3bc09d9
orestisf <orestisf1993@gmail.com>
2016-01-07 18:21:36 +0200

    Improve plot_bars.m

--- Matlab_Files/plot_bars.m
+++ Matlab_Files/plot_bars.m
@@ -1,49 +1,51 @@
-function plot_bars(c,tit,name)
+function plot_bars(c, tit, name)
 % set(0, 'currentfigure', h); % set current figure
 % hold on; <<--- for some reasons breaks the colors for me.
 % axes = h.CurrentAxes;
 
-dMap = {'COS'; 'COR'};
-lMet = {'WE'; 'WA'; 'SI'; 'CO'; 'AV'};
+lMethod = {'weighted'; 'ward'; 'complete'; 'average'};
 
-% set(gca, 'XTick',1:10, 'XTickLabel',{l{1} l{2} 'C' 'D' 'E'})
-% c(k,:) = [DATASET,DIST,MERTHOD,EVAL1,EVAL2,sil,coh,sep];
-
-L = size(c,1);
+L = size(c, 1);
 figure('visible', 'off', 'PaperType', 'a4', 'PaperOrientation', 'portrait', ...
   'PaperUnits', 'centimeters', 'PaperPosition', [0 0 21 29.7], 'PaperPositionMode', 'manual', ...
   'Menubar', 'none', 'defaulttextinterpreter', 'latex', 'units', 'normalized', 'outerposition', [0 0 1 1]);
-y = [c(:,6),c(:,7),c(:,8),c(:,5)];
+y = [c(:, 6), c(:, 7), c(:, 8), c(:, 5)];
 barh(y)
 
 paths_filename = '../2nd-assignment/datasets/paths.txt';
 files = file_paths(paths_filename);
+files = strrep(files(:), '../2nd-assignment/datasets/root/', '');
+files = strrep(files(:), '_', '\_');
 
-legend('Silhouette','Cohesion','Separation','Success\_Rate')
-x = cell(1,L);
-for t = 1:1:L
-    x{t} = [files{(c(t, 1))} ,dMap{c(t, 2)}, lMet{c(t, 3)}];
+legend('Silhouette', 'Cohesion', 'Separation', 'Success\_Rate')
+
+P = findobj(gca, 'type', 'patch');
+C = ['w', 'k', 'm', 'g']; % color list
+for n = 1:length(P)
+    set(P(n), 'facecolor', C(n));
 end
-% axis([ 0 inf 0 1 ]);
 
-
-% set(gca, 'YTick', 1:L, 'YTickLabel', x)
-% set(gca, 'YTickLabelRotation', 90)
-
-
-
-% set(gca,'XTickLabelRotation',90)
-
+x = cell(1, L);
+for t = 1:1:L
+    x{t} = [files{(c(t, 1))}, '-', lMethod{c(t, 3)}];
+end
 
 title(tit)
 
-
-set(gca, 'YTick',1:L, 'YTickLabel',x)
+set(gca, 'YTick', 1:L, 'YTickLabel', x)
+% FontName may break some stuff if you don't  have them.
+% set(gca, 'FontName', 'Source Code Pro Medium')
 set(gca, 'FontSize', 3)
+
+ax = gca;
+ax.Title.FontSize = 10;
+
+% set colors for bars
+cat = ax.Children;
+set(cat(1), 'FaceColor', [230, 97, 1] / 255, 'EdgeColor', [230, 97, 1] / 255);
+set(cat(2), 'FaceColor', [253, 184, 99] / 255, 'EdgeColor', [253, 184, 99] / 255);
+set(cat(3), 'FaceColor', [178, 171, 210] / 255, 'EdgeColor', [178, 171, 210] / 255);
+set(cat(4), 'FaceColor', [94, 60, 153] / 255, 'EdgeColor', [94, 60, 153] / 255);
+
 print(name, '-dpdf', '-r0')
-
-
-
-close all
-
 end
